e5876cbd20a42c7a5108987887c30f1044bb2e34
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2024-04-05 16:15:14 -0300

    instantiated prisma client.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const prismaClient = require("@prisma/client");
 
+const prisma = new prismaClient();
+
+async function main() {}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
